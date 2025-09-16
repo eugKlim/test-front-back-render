7838c271248f01e3cbb61fb24ad11920b0e9f152
Eugene <eugklim1@gmail.com>
2025-09-16 19:31:14 +0300

    first commit 5

--- src/shared/api/http/index.ts
+++ src/shared/api/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = import.meta.env.VITE_API_BASE_URL as string;
+export const API_URL = import.meta.env.PUBLIC_API_BASE_URL || import.meta.env.VITE_API_BASE_URL;
 // export const API_URL = `https://server-avgm.onrender.com/api`;
 // export const API_URL = `http://localhost:5000/api`;
 
