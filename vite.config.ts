f009fffa8618ddb5165237c4f18609d7cb7791fe
Eugene <eugklim1@gmail.com>
2025-09-16 21:33:41 +0300

    test del server from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://server-avgm.onrender.com',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://server-avgm.onrender.com',
+  //       changeOrigin: true,
+  //       secure: true,
+  //     },
+  //   },
+  // }
 })
